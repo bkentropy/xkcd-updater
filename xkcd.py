6c322a153c128cfa4f3f323a13ec9962eb7a49ba
Brian Kustra <bkustrakb@gmail.com>
2018-01-22 11:04:41 -0600

    Must provide hipchat url to post comic

--- xkcd.py
+++ xkcd.py
@@ -1,6 +1,8 @@
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 import urllib.request
 import re
+import requests
+import sys
 
 response = urllib.request.urlopen("https://xkcd.com")
 html = response.read()
@@ -12,5 +14,22 @@ div7 = divs[7]
 link = div7.contents[10]
 imgurl = div7.contents[12]
 
-print(re.search(r"(https://.*)\n", imgurl).groups()[0])
+title = "MWF XKCD"
+src = re.search(r"(https://.*)\n", imgurl).groups()[0]
+
+url = str(sys.argv[1])
+payload = {
+    "color": "gray",
+    "message": "<span>" + title + "</span><br><img src='" + src + "'/>",
+    "notify": True,
+    "message_format": "html"
+}
+
+print("Posting...")
+if url:
+    r = requests.post(url, data=payload)
+    print("Posted!")
+else:
+    print("Must provide hipchat URL as command line argument")
+
 
