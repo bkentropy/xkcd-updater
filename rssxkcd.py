f90ab1d154c7837413aa0c2008ca41c503784515
Brian Kustra <bkustrakb@gmail.com>
2018-01-29 08:31:15 -0600

    Made RSSEntry class, now insert into feed.db

--- rssxkcd.py
+++ rssxkcd.py
@@ -23,10 +23,50 @@ def post_in_db(title):
 # curl -I https://xkcd.com/rss.xml
 # curl -I --header 'If-Modified-Since: Fri, 26 Jan 2018 15:28:35 GMT' https://xkcd.com/rss.xml
 
+#get a {
+#   title
+#   img
+#   alttext
+#}
+
 # get the feed data from the url
 feed = feedparser.parse(url)
+entries = feed.entries
+keys = entries[0].keys()
 
-print(feed)
+titles = [entry["title"] for entry in entries]
+imglinks = [entry["summary"].split("\"")[1] for entry in entries]
+summaries = [entry["summary"].split("\"")[3] for entry in entries]
+links = [entry["link"] for entry in entries]
+published = [entry["published"] for entry in entries] 
+
+class Entry:
+    def __init__(self, title, imglink, summary, link, pubts):
+        self.title = title
+        self.imglink = imglink
+        self.summary = summary
+        self.link = link
+        self.pubts = pubts
+
+    def analyze():
+        data = "Title:" + self.title + "\n"
+        data += "Imglink:" + self.imglink + "\n"
+        data += "Summary:" + self.summary + "\n"
+        data += "Link:" + self.link + "\n"
+        data += "Pubts:"+ self.pubts
+        return data
+
+RSSEntries = []
+for i in range(len(titles)):
+    e = Entry(titles[i], imglinks[i], summaries[i], links[i], published[i])
+    e.analyze()
+    RSSEntries.append(e)
+
+
+    
+
+
+
 #>>> rssxkcd.feed.entries[0].keys()
 #dict_keys(['title', 'title_detail', 'links', 'link', 'summary',
 #    'summary_detail', 'published', 'published_parsed', 'id', 'guidislink'])
