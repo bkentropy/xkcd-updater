bd70f02b8e12a3d511d2aed7a5801451db8bf9ea
Brian Kustra <bkustrakb@gmail.com>
2018-01-30 15:44:57 -0600

    Added some sqlite stuff, not sure about how synced up it is

--- rssxkcd.py
+++ rssxkcd.py
@@ -2,8 +2,8 @@
 
 import feedparser
 import time
-from subprocess import check_output
 import sys
+import sqlite3
 
 feed_name = 'Xkcd!'
 url = 'https://xkcd.com/rss.xml'
@@ -23,12 +23,6 @@ def post_in_db(title):
 # curl -I https://xkcd.com/rss.xml
 # curl -I --header 'If-Modified-Since: Fri, 26 Jan 2018 15:28:35 GMT' https://xkcd.com/rss.xml
 
-#get a {
-#   title
-#   img
-#   alttext
-#}
-
 # get the feed data from the url
 feed = feedparser.parse(url)
 entries = feed.entries
@@ -38,15 +32,15 @@ titles = [entry["title"] for entry in entries]
 imglinks = [entry["summary"].split("\"")[1] for entry in entries]
 summaries = [entry["summary"].split("\"")[3] for entry in entries]
 links = [entry["link"] for entry in entries]
-published = [entry["published"] for entry in entries] 
+published = [entry["published"] for entry in entries]
 
 class Entry:
     def __init__(self, title, imglink, summary, link, pubts):
         self.title = title
         self.imglink = imglink
         self.summary = summary
-        self.link = link
-        self.pubts = pubts
+        self.link = link # this will be the id field
+        self.pubts = pubts # set sqlite3 to text, change to timestamp in some way
 
     def analyze():
         data = "Title:" + self.title + "\n"
@@ -59,11 +53,34 @@ class Entry:
 RSSEntries = []
 for i in range(len(titles)):
     e = Entry(titles[i], imglinks[i], summaries[i], links[i], published[i])
-    e.analyze()
+    #e.analyze()
     RSSEntries.append(e)
 
+db = sqlite3.connect("feed.db")
+cursor = db.cursor()
 
-    
+# ALGO:
+# for get from RSS include the optional updated since flag
+# check if link is in DB
+#   if not add it
+#   and post it
+#   else it was already saved and already posted
+
+def insert_entries(RSSEntries):
+    for e in RSSEntries:
+        cursor.execute('''INSERT INTO entries(id, title, imglink, summary, pubts)
+        VALUES(?,?,?,?,?)''', (e.link, e.title, e.imglink, e.summary, e.pubts))
+        print("Entered...")
+    db.commit()
+
+cursor.execute('''SELECT id FROM entries where id=?''', (link,))
+all_rows = cursor.fetchall()
+def printrows(rows):
+    print("Id   :   Title   :   Imglink     :   Summary     :   Published")
+    for row in rows:
+        print('{0} : {1} : {2} : {3} : {4}'.format(row[0], row[1], row[2], row[3], row[4]))
+
+
 
 
 
