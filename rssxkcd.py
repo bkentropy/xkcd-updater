29e0b30ea4fb43f8808b11ee8e24bc1e400b04cd
Brian Kustra <bkustrakb@gmail.com>
2018-02-01 17:02:30 -0600

    almost there. the checking isn't quite right

--- rssxkcd.py
+++ rssxkcd.py
@@ -11,19 +11,21 @@ url = 'https://xkcd.com/rss.xml'
 #url = sys.argv[2]
 
 class Entry:
-    def __init__(self, title, imglink, summary, link, pubts):
+    def __init__(self, title, imglink, summary, link, pubts, posted):
         self.title = title
         self.imglink = imglink
         self.summary = summary
         self.link = link # this will be the id field
         self.pubts = pubts # set sqlite3 to text, change to timestamp in some way
+        self.posted = 0
 
     def analyze(self):
         data = "Title:" + self.title + "\n"
         data += "Imglink:" + self.imglink + "\n"
         data += "Summary:" + self.summary + "\n"
         data += "Link:" + self.link + "\n"
-        data += "Pubts:"+ self.pubts
+        data += "Pubts:" + self.pubts + "\n"
+        data += "Posted:" + self.posted
         return data
 
 # ALGO:
@@ -39,12 +41,11 @@ row = cursor.execute("SELECT id from lastpub")
 lastts = row.fetchone()
 
 req = requests.get(url, headers={
-    "If-Modified-Since": lastts
+    "If-Modified-Since": str(lastts)
 })
 
+RSSEntries = []
 if req.text:
-    print(req)
-
     # get the feed data from the url
     feed = feedparser.parse(url)
     entries = feed.entries
@@ -56,29 +57,54 @@ if req.text:
     links = [entry["link"] for entry in entries]
     published = [entry["published"] for entry in entries]
 
-RSSEntries = []
-for i in range(len(titles)):
-    e = Entry(titles[i], imglinks[i], summaries[i], links[i], published[i])
-    #e.analyze()
-    RSSEntries.append(e)
+    for i in range(len(titles)):
+        e = Entry(titles[i], imglinks[i], summaries[i], links[i], published[i], 0)
+        #e.analyze()
+        RSSEntries.append(e)
 
 def insert_entry(db, cursor, e):
-    cursor.execute('''INSERT INTO entries(id, title, imglink, summary, pubts)
-    VALUES(?,?,?,?,?)''', (e.link, e.title, e.imglink, e.summary, e.pubts))
+    cursor.execute('''INSERT INTO entries(id, title, imglink, summary, pubts, posted)
+    VALUES(?,?,?,?,?,?)''', (e.link, e.title, e.imglink, e.summary, e.pubts, 0))
     print("Entered...")
     db.commit()
 
-cursor.execute('''SELECT id FROM entries where id=?''', (link,))
-all_rows = cursor.fetchall()
-def printrows(rows):
-    print("Id   :   Title   :   Imglink     :   Summary     :   Published")
-    for row in rows:
-        print('{0} : {1} : {2} : {3} : {4}'.format(row[0], row[1], row[2], row[3], row[4]))
+def check_if_in_db(db, cursor, e):
+    rc = cursor.execute('''SELECT id FROM entries where id=?''', (e.link,))
+    if rc.fetchone():
+        return True
+    else:
+        return False
+
+def check_if_posted(db, cursor, e):
+    rc = cursor.execute('''SELECT id FROM entries where posted=?''', (e.posted,))
+    if rc.fetchone():
+        return True
+    else:
+        return False
+
+def check_and_post(db, cursor, ents):
+    for e in ents:
+        indb = check_if_in_db(db, cursor, e)
+        if indb:
+            posted = check_if_posted(db, cursor, e)
+            if not posted:
+                # post! to hipchat
+                print("post!")
+        else:
+            insert_entry(db, cursor, e)
+            # post! to hipchat
+            print("post!")
+
+if len(RSSEntries) > 0:
+    print("nice")
+    check_and_post(db, cursor, RSSEntries)
 
 
-
-
-
+#all_rows = cursor.fetchall()
+#def printrows(rows):
+#    print("Id   :   Title   :   Imglink     :   Summary     :   Published")
+#    for row in rows:
+#        print('{0} : {1} : {2} : {3} : {4}'.format(row[0], row[1], row[2], row[3], row[4]))
 #>>> rssxkcd.feed.entries[0].keys()
 #dict_keys(['title', 'title_detail', 'links', 'link', 'summary',
 #    'summary_detail', 'published', 'published_parsed', 'id', 'guidislink'])
