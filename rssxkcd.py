52e88f8e7884f62ee2a10f3fa87a5e3a285e564a
Brian Kustra <bkustrakb@gmail.com>
2018-02-02 15:08:37 -0600

    everything is testing well in the lab, needs to test in the field

--- rssxkcd.py
+++ rssxkcd.py
@@ -23,16 +23,18 @@ class Entry:
         data += "Posted:" + str(self.posted)
         print(data)
 
+# Get rss feed from URL (https://xkcd.com/rss.xml)
 def check_rss_feed(cursor, url, rssentries):
-    row = cursor.execute("SELECT id from lastpub")
+    row = cursor.execute("SELECT id FROM lastpub")
     lastts = row.fetchone()
     req = requests.get(url, headers={
-        "If-Modified-Since": str(lastts)
+        "If-Modified-Since": lastts[0]
     })
     if req.text:
         # get the rss feed data from the url
         feed = feedparser.parse(url)
         entries = feed.entries
+# TODO: refactor this, maybe just one pass over entries
         titles = [entry["title"] for entry in entries]
         imglinks = [entry["summary"].split("\"")[1] for entry in entries]
         summaries = [entry["summary"].split("\"")[3] for entry in entries]
@@ -51,30 +53,23 @@ def post_to_hipchat(title, src, hipurl):
         "notify": True,
         "message_format": "html"
     }
-
-    if hipurl:
-        #r = requests.post(hipurl, data=payload)
-        print(title, "Posted!")
-    else:
-        print("Must provide hipchat URL as command line argument")
-
-
+    r = requests.post(hipurl, data=payload)
+    print(title, "Posted!")
 
 # Database functions
 def insert_entry(db, cursor, e):
     cursor.execute('''INSERT INTO entries(id, title, imglink, summary, pubts, posted)
     VALUES(?,?,?,?,?,?)''', (e.link, e.title, e.imglink, e.summary, e.pubts, 0))
     db.commit()
-    print("Entered...")
+    print("Saved entry in db")
 
 def update_to_posted(db, cursor, e):
     cursor.execute('UPDATE entries SET posted=1 WHERE id=?', (e.link,))
     db.commit()
     print("Updated posted for:", e.link)
 
-
 def check_if_in_db(db, cursor, e):
-    rc = cursor.execute('''SELECT id FROM entries WHERE id=?''', (e.link,))
+    rc = cursor.execute('SELECT id FROM entries WHERE id=?', (e.link,))
     if rc.fetchone():
         return True
     else:
@@ -88,7 +83,9 @@ def check_if_posted(db, cursor, e):
     else:
         return False
 
+# Primary function
 def check_and_post(db, cursor, ents):
+    # TODO: lines 96-99 and 102-106 are ripe for refactor
     update_timestamp = False
     for e in ents:
         print("title:", e.title, "posted:", e.posted)
@@ -97,14 +94,14 @@ def check_and_post(db, cursor, ents):
             posted = check_if_posted(db, cursor, e)
             if not posted:
                 title = e.title + " (" + str(e.link) + ")"
-                #post_to_hipchat(title, e.imglink, hipurl)
+                post_to_hipchat(title, e.imglink, hipurl)
                 update_to_posted(db, cursor, e)
                 update_timestamp = True
                 print("not in db or posted")
         else:
             insert_entry(db, cursor, e)
             title = e.title + " (" + str(e.link) + ")"
-            #post_to_hipchat(title, e.imglink)
+            post_to_hipchat(title, e.imglink)
             update_to_posted(db, cursor, e)
             update_timestamp = True
             print("not in db at all")
@@ -115,11 +112,11 @@ def main():
     url = 'https://xkcd.com/rss.xml'
     hipurl = str(sys.argv[1])
     RSSEntries = []
-
     db = sqlite3.connect("feed.db")
     cursor = db.cursor()
 
-    req = check_rss_feed(cursor, url, RSSEntries)
+    if url and hipurl:
+        req = check_rss_feed(cursor, url, RSSEntries)
 
     if len(RSSEntries) > 0:
         need_update_timestamp = check_and_post(db, cursor, RSSEntries)
@@ -127,6 +124,8 @@ def main():
             newts = (req.headers["Last-Modified"],)
             cursor.execute("UPDATE lastpub set id=?", newts)
             db.commit()
+    else:
+        print("All up to date!")
 
 if __name__ == "__main__":
     main()
